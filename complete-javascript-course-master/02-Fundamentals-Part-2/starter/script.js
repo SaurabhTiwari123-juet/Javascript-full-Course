1acc668c37515e3c0478ec03c345503066ca8875
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2022-10-17 02:18:35 +0530

    new1

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,8 @@
 "use strict";
 // it help us to find errors and makes code useful to run
+let hasdriverslicence = false;
+const passtest = true;
+
+if (passtest) hasdriverlicence = true;
+
+if (hasdriverslicence) console.log("i can drive");
