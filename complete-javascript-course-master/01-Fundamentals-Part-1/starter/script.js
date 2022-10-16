e65a7455b4172114e9e7692c52a8ec18ea63ff29
SaurabhTiwari123-juet <56027820+SaurabhTiwari123-juet@users.noreply.github.com>
2022-10-17 02:14:03 +0530

    new

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,10 @@
+ let js = 'amazing';
+    if (js === 'amazing') {
+      //alert('javascript is fun!');
+    }
+    console.log("sau");
+    console.log(23);
+
+    let firstName="saurabh";// CAMEL CASE WITHING STANDARD IN JS
+// variables must be declaritive
+    console.log(firstname);
---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,2 @@
+"use strict";
+// it help us to find errors and makes code useful to run
